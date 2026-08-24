efe79f122e6b0fb2d35bc99889c057f9c1357a2b
DavidP555 <sancdavid.555@gmail.com>
2026-08-24 11:05:51 -0500

    docs: add JSDoc comments to controllers index

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,3 +1,8 @@
+/**
+ * @fileoverview Barrel de controladores.
+ * Punto unico de exportacion de los hooks controladores (capa
+ * Controller) usados por las vistas.
+ */
 export { useProductController } from './use-product-controller';
 export { useCartController } from './use-cart-controller';
 export { useAuthController } from './use-auth-controller';
