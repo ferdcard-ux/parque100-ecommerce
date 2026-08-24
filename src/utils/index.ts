10aea899726abc4c28ec6b6c244f885f90f81378
DavidP555 <sancdavid.555@gmail.com>
2026-08-24 11:39:21 -0500

    docs(utils): add JSDoc comments to utils index

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,7 @@
+/**
+ * @fileoverview Barrel de utilidades.
+ * Punto unico de exportacion de constantes, formatters y validadores.
+ */
 export * from './constants';
 export * from './formatters';
 export * from './validators';
